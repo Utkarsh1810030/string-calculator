c6004c18643ae51c7b1853f3049abc3c5f5c2c3b
Utkarsh Chaudhar <utkarshc@Utkarshs-MacBook-Pro.local>
2025-08-30 22:17:08 +0530

    feat: minimal add() base condition added

---
+++ src/stringCalculator.js
@@ -0,0 +1,6 @@
+function add(input) {
+    if (input === "") return 0;
+    return 0;
+}
+
+module.exports = { add };
---
+++ test/stringCalculator.test.js
@@ -0,0 +1,7 @@
+const { add } = require("../src/stringCalculator");
+
+describe("String Calculator", () => {
+    test("empty string returns 0", () => {
+        expect(add("")).toBe(0);
+    });
+});
